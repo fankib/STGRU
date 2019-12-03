02152aadb97164937922a8647e56cfbba75c8b88
Benjamin Fankhauser <benjamin.fankhauser@bfh.ch>
2019-12-03 22:29:37 +0100

    fixup

--- gru/train.py
+++ gru/train.py
@@ -14,7 +14,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--gpu', default=-1, type=int, help='the gpu to use')
 parser.add_argument('--users', default=10, type=int, help='users to process')
 parser.add_argument('--dims', default=10, type=int, help='hidden dimensions to use')
-parser.add_arguemnt('--seq_length', default=10, type=int, help='seq-length to process in one pass')
+parser.add_argument('--seq_length', default=10, type=int, help='seq-length to process in one pass')
 args = parser.parse_args()
 
 ###### parameters ######
